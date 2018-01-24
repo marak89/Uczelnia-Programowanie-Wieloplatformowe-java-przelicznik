42e46cba8b5e2b82b4a7129ba7e261ec5b539612
Marcin Rał <marak89@marak89.com>
2018-01-24 21:10:18 +0100

    first commit

---
+++ Przelicznik1.java
@@ -0,0 +1,16 @@
+/**
+ * Oprogramowanie stworzył
+ * @author Marcin Rał
+ * jako projekt zaliczeniowy z przedmiotu Programowanie Wieloplatformowe
+ * 
+ */
+package przelicznik1;
+
+import javax.swing.JApplet;
+
+public class Przelicznik1 extends JApplet {
+    public static void main(String[] args) {
+        loginForm lf = new loginForm();
+        lf.setVisible(true);
+    }
+}
